de5f944f0b2b38fdba614f29668e884f9234afa9
windrosecn <windrosecn@gmail.com>
2025-02-11 14:50:20 +0800

    use qwen2.5-coder:7b

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -15,7 +15,7 @@ return {
           __inherited_from = 'openai',
           api_key_name = '',
           endpoint = 'http://127.0.0.1:11434/v1',
-          model = 'deepseek-r1:7b',
+          model = 'qwen2.5-coder:7b',
         },
       },
     },
